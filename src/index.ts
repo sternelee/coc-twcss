ddbe040568567d7e15178ae6c290384c96f3de5e
李兴广 <lixingguang@xunlei.com>
2022-10-16 22:09:08 +0800

    feat: 支持配置

--- src/index.ts
+++ src/index.ts
@@ -3,77 +3,47 @@ import { commands, CompleteResult, ExtensionContext, listManager, sources, windo
 import { twcssItems } from './tw';
 // import fs from 'fs';
 // import path from 'path';
-// import DemoList from './lists';
-
-// const CONFIG_FILE_GLOB = '{tailwind,tailwind.config,windicss.config,unocss.config}.{js,ts,cjs}';
 
 const items = Object.keys(twcssItems).map((k) => ({ word: k, info: twcssItems[k], kind: 'TW' }));
 
+let isTwind = false;
+let isUnoCSS = false;
+
 export async function activate(context: ExtensionContext): Promise<void> {
+  const isEnabled = workspace.getConfiguration('coc-twcss').get<boolean>('enabled', false);
+  isTwind = workspace.getConfiguration('coc-twcss').get<boolean>('twind', false);
+  isUnoCSS = workspace.getConfiguration('coc-twcss').get<boolean>('unocss', false);
+  if (!isEnabled) return;
   window.showMessage(`coc-twcss works!`);
 
-  // const file = path.resolve(__dirname, 'tw.json');
-  // if (!fs.existsSync(file)) return;
-
-  // fs.readFile(file, 'utf8', (err, content) => {
-  //   if (err) return;
-  //   const lines = JSON.parse(content);
-  //   Object.keys(lines).forEach((key: string) => {
-  //     twcssItems.push({ description: lines[key], character: key });
-  //   });
-  // });
-
   context.subscriptions.push(
-    // commands.registerCommand('coc-twcss.Command', async () => {
-    //   window.showMessage(`coc-twcss Commands works!`);
-    // }),
-
-    // listManager.registerList(new DemoList(workspace.nvim)),
-
     sources.createSource({
       name: 'coc-twcss', // unique id
       triggerCharacters: [],
-      // shouldComplete: async (opt) => {
-      //   const { linenr, col, input, line } = opt
-      //   const buf = Buffer.from(line, 'utf8');
-      //   const pre = buf.slice(0, col - 1).toString('utf8');
-      //   // let after = buf.slice(col + input.length).toString('utf8');
-      //   // TODO: 只支持windicss这种class内的填写
-      //   // const hasItems = /<[^<]*?class="[^"]*?\s$/g.test(pre);
-      //   // TODO：同时支持twind
-      //   // const hasItems = /<[^<]*?class="[^"]*?\s$/g.test(pre) || /tw\`/gi.test(pre);
-      //   // 支持在unocss下的通用
-      //   const hasItems = /<[^<]*?\s$/g.test(pre);
-      //   window.showMessage('coc-twcc: ' + pre + ' -> ' + hasItems);
-      //   return hasItems;
-      // },
-      doComplete: async (opt) => {
-        const { linenr, col, input, line } = opt
+      shouldComplete: async (opt) => {
+        const { col, line } = opt;
         const buf = Buffer.from(line, 'utf8');
         const pre = buf.slice(0, col).toString('utf8');
-        const hasItems = /<[^<]*?[\s|"]$/g.test(pre);
-        if (!hasItems) return null
-        const items = await getCompletionItems();
+        let hasItems = /<[^<]*?class="[^"]*?\s$/g.test(pre);
+        if (isTwind) {
+          hasItems = hasItems || /tw\`/gi.test(pre);
+        }
+        if (isUnoCSS) {
+          hasItems = hasItems || /<[^<]*?[\s|"]$/g.test(pre);
+        }
+        return hasItems;
+      },
+      doComplete: async () => {
+        let items = await getCompletionItems();
+        if (isTwind) {
+          items = items.map((v) => ({ ...v, info: `Twind: \n ${v.info}` }));
+        }
+        if (isUnoCSS) {
+          items = items.map((v) => ({ ...v, info: `UnoCSS: \n ${v.inf}` }));
+        }
         return items;
       },
-    }),
-
-    // workspace.registerKeymap(
-    //   ['n'],
-    //   'twcss-keymap',
-    //   async () => {
-    //     window.showMessage(`registerKeymap`);
-    //   },
-    //   { sync: false }
-    // ),
-
-    // workspace.registerAutocmd({
-    //   event: 'InsertLeave',
-    //   request: true,
-    //   callback: () => {
-    //     window.showMessage(`registerAutocmd on InsertLeave`);
-    //   },
-    // })
+    })
   );
 }
 
@@ -81,15 +51,5 @@ async function getCompletionItems(): Promise<CompleteResult> {
   return {
     priority: 1024,
     items,
-    // items: [
-    //   {
-    //     word: 'TestCompletionItem 1',
-    //     menu: '[coc-twcss]',
-    //   },
-    //   {
-    //     word: 'TestCompletionItem 2',
-    //     menu: '[coc-twcss]',
-    //   },
-    // ],
   };
 }
